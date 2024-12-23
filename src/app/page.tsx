48f3dc9e3d12cf0a8bb07f359293bd1262f5b2cf
Diery Dia <dierydia@esp.sn>
2024-12-23 16:24:32 +0000

    fix:gap for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -235,7 +235,7 @@ export default function Home() {
                 Compétences
               </h2>
             </div>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 justify-items-center">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-12 justify-items-center">
               {profile.skills.map((skill, index) => (
                 <motion.div
                   key={skill}
